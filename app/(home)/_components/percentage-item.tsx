b9a7d33dc16d431b24d4df34b160c1efc8151f53
Gabriel-Mori <gabrielmmori51@outlook.com>
2025-04-07 10:06:53 -0300

    fix: update dates e value percentage

--- app/(home)/_components/percentage-item.tsx
+++ app/(home)/_components/percentage-item.tsx
@@ -7,6 +7,11 @@ interface PercentageItemProps {
 }
 
 const PercentageItem = ({ icon, title, value }: PercentageItemProps) => {
+  let valeuPercentage = value;
+  if (Number.isNaN(value) || value === undefined || value === null) {
+    valeuPercentage = 0;
+  }
+
   return (
     <div className="flex items-center justify-between">
       {/* Icone */}
@@ -14,7 +19,7 @@ const PercentageItem = ({ icon, title, value }: PercentageItemProps) => {
         <div className="rounded-lg bg-white bg-opacity-[3%] p-2">{icon}</div>
         <p className="text-sm text-muted-foreground">{title}</p>
       </div>
-      <p className="text-sm font-bold">{value}%</p>
+      <p className="text-sm font-bold">{valeuPercentage}%</p>
     </div>
   );
 };
